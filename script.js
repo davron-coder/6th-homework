9da064a7caf41ab9f48f3cca19f397fd7a1a7f72
Давронбек Санатиллоев <samir2q8@gmail.com>
2024-12-14 20:07:33 +0500

    first commit

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
+let arr = [
+    [115,50,25],
+    [75,12,99],
+    [34,36,55],
+];
 
-let text = "Lorem ipsum dolor, sit amet consectetur adipisicing elit, Officiis, nostrum,";
-let newText = text.split(" , ".join("."));
-console.log("Измененная строка:", newText);
+let result = arr.map(Arr => Arr.reduce((num1, num2) => num1 + num2)); 
+console.log(result);
 
-// Заменить все запятые на точки
+// нужно сложить числа внутри вложенных массивов
+// ответ должен выглядить так - [190,186,125]
 
+let prices = [15000,3200,10200,455000,123000,7000];
+let min = Math.min(...prices)
+let max = Math.max(...prices)
 
-let randomNumber = Math.floor(Math.random() * 500); 
-console.log("Случайное число до 500:", randomNumber);
+console.log(
+    "min " + min
+);
 
-// генерация рандомных чисел до 500
+console.log(
+    "max " + min
+);
 
-let arr = [0, 123, -3, 43, 22, -16, 12, 99, 100];
-let max1 = Math.max(...arr); 
-console.log("Наибольшее число в массиве:", max1);
+// нужно найти макс и мин price 
+// reduce применять нельза!
 
-let arr1 = [10.45, 9.519, 85.56, 22, 6.26, 98.245, 15, 34.2];
+let nums = [12,3,57,34,90,1,10,9];
+let { odd, even } = nums.reduce((acc, num) => {
+    if (num % 2 === 0) {
+      acc.even.push(num);
+    } else {
+      acc.odd.push(num);
+    }
+    return acc;
 
-// достать наибольшее число с массива, сохранить в max      
-let min = Math.round(Math.min(...arr1));
-console.log("Наименьшее число после округления:", min);
+}, {odd: [] , even:[]});
+console.log(even);
+console.log(odd);
 
-
-let max = Math.ceil(Math.max(...arr));
-console.log("Наибольшее число после округления вверх:", max);
-
-
-let result = max % min;
-console.log("Результат деления с остатком:", result);
-
-// Достать наименьшее число с массива, округлить логически и сохранить в min  
-// достать наибольшее число с массива, округлить вверх и сохранить в max
-// поделить с остатком max на min, показать результат
-
-let num1 = 10.4568313123132;
-
-
-let numStr = num1.toFixed(3);
-console.log("Строка с тремя знаками после точки:", numStr);
-
-let length = numStr.length;
-console.log("Длина строки:", length);
-
-let numInt = Math.floor(Number(numStr));
-console.log("Целое число без дробной части:", numInt);
-
-// 1. num1 переводим в string, оставляем только три цифры после точки: '10.456'
-// 2. показать длину полученной строки 
-// 3. полученную строку переводим в тип number оставляя лишь целое число (убрать все цифры после точки)
+// в odd сохранить нечетные числа 
+// в even сохранить четные числа 
+// filter использовать нельзя
